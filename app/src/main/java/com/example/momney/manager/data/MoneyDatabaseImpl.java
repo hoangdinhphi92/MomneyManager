1c143c4e081c9dd4ffa44720387a99b57783b44e
ducvitqp12 <66892883+ducvitqp12@users.noreply.github.com>
2021-05-01 21:19:56 +0700

    Create Database

--- app/src/main/java/com/example/momney/manager/data/MoneyDatabaseImpl.java
+++ app/src/main/java/com/example/momney/manager/data/MoneyDatabaseImpl.java
@@ -1,6 +1,20 @@
 package com.example.momney.manager.data;
 
-public class MoneyDatabaseImpl implements MoneyDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+
+public class MoneyDatabaseImpl extends SQLiteOpenHelper implements MoneyDatabase {
+
+    public static final String DATABASE_NAME = "money.db";
+
+    public MoneyDatabaseImpl(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
     @Override
     public void insert(MoneyEntry money) {
@@ -14,6 +28,20 @@ public class MoneyDatabaseImpl implements MoneyDatabase {
 
     @Override
     public void delete(MoneyEntry money) {
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + DATABASE_NAME + " (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "amount INTEGER," +
+                "time INTEGER," +
+                "description TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 }
