fbb89a7ff3b371a7efb4eb7a4e5f79cbae480738
ducvitqp12 <66892883+ducvitqp12@users.noreply.github.com>
2021-05-03 21:19:51 +0700

    Insert, update, delete

--- app/src/main/java/com/example/momney/manager/data/MoneyDatabaseImpl.java
+++ app/src/main/java/com/example/momney/manager/data/MoneyDatabaseImpl.java
@@ -7,28 +7,38 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 
-
 public class MoneyDatabaseImpl extends SQLiteOpenHelper implements MoneyDatabase {
 
     public static final String DATABASE_NAME = "money.db";
+    public static final int DATABASE_VESION = 1;
 
-    public MoneyDatabaseImpl(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + DATABASE_NAME;
+
+    public MoneyDatabaseImpl(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VESION);
     }
 
     @Override
     public void insert(MoneyEntry money) {
-
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("INSERT INTO " + DATABASE_NAME + " VALUES (" +
+                "null, "+ money.getAmount() + ", " +
+                money.getTime() + ", '" + money.getDescription() + "')");
     }
 
     @Override
     public void update(MoneyEntry money) {
-
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("UPDATE "+ DATABASE_NAME +" SET " +
+                "amount ="+ money.getAmount() + ", time = " + money.getTime() +
+                " description = '" + money.getDescription() + "' WHERE id = " + money.getId() );
     }
 
     @Override
     public void delete(MoneyEntry money) {
-
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + DATABASE_NAME + " WHERE id = " + money.getId());
     }
 
     @Override
@@ -42,6 +52,8 @@ public class MoneyDatabaseImpl extends SQLiteOpenHelper implements MoneyDatabase
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
     }
+
 }
